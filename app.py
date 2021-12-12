1b936b445313331042c7276993f2ba2aae88bc1b
keie <hombrehealth@hotmail.com>
2021-12-11 22:40:32 -0400

    changes

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 from flask import Flask, request, Response, jsonify, json, send_from_directory, abort
 import os
+from keras.models import load_model
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-	return 'Hello IIS from Flask framework. :D'
+	return 'Hello IIS from Flask framework. :D 2'
 
 @app.route('/Hello')
 def hello_world():
